d2d06e84299a83a0b6e5b1a4ca41a50699552580
Gaurav Verma <114090891+briskgaurav@users.noreply.github.com>
2023-01-21 21:38:18 +0530

    Create BasicDataTypes.cpp

---
+++ HackerRank/C++/BasicDataTypes.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e; 
+     cout<<a<<endl;
+     cout<<b<<endl;
+     cout<<c<<endl;
+     cout<<fixed<<setprecision(3)<<d<<endl; //for fixed number of decimal digits eg. 123.333
+     cout<<fixed<<setprecision(9)<<e<<endl;  //for fixed number of decimal digits eg. 123.333333333
+    return 0;
+}
