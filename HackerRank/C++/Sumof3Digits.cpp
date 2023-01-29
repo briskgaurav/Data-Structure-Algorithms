7337eb5fd0203ea046f9d99160fd37495f2249e2
Gaurav Verma <114090891+briskgaurav@users.noreply.github.com>
2023-01-29 21:05:09 +0530

    Create Sumof3Digits.cpp

---
+++ HackerRank/C++/Sumof3Digits.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+  float a;
+  cout<<"a is :"<<endl;
+  cin>>a;
+  float b;
+    cout<<"b is :"<<endl;
+    cin>>b;
+  float c;
+    cout<<"c is :"<<endl;
+  cin>>c;
+  cout<<a+b+c;
+
+return 0;
+
+}
