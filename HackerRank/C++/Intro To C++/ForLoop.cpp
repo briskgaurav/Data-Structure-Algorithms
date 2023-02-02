5db54e5219757deaf2bfa4b28a2f89c9af786251
Gaurav Verma <114090891+briskgaurav@users.noreply.github.com>
2023-02-02 14:28:01 +0530

    Create ForLoop.cpp

---
+++ HackerRank/C++/Intro To C++/ForLoop.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n,b;
+    cin>>n>>b;
+    for(int a=n;a<=b;a++){
+        if(a==1){
+            cout<<"one"<<endl;
+        }
+        else if(a==2){
+            cout<<"two"<<endl;
+        }
+        else if(a==3){
+            cout<<"three"<<endl;
+        }
+        else if(a==4){
+            cout<<"four"<<endl;
+        }
+        else if(a==5){
+            cout<<"five"<<endl;
+        }
+        else if(a==6){
+            cout<<"six"<<endl;
+        }
+        else if(a==7){
+            cout<<"seven"<<endl;
+        }
+        else if(a==8){
+            cout<<"eight"<<endl;
+        }
+        else if(a==9){
+            cout<<"nine"<<endl;
+        }
+        else if (a%2==1){
+            cout<<"odd"<<endl;
+        
+        }
+        else{
+            cout<<"even"<<endl;
+        }
+    
+    }
+    
+    return 0;
+}
