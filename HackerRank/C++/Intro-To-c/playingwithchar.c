6f9efbc97abc362c530bdc6f561e38cd07cc48b7
Gaurav Verma <114090891+briskgaurav@users.noreply.github.com>
2023-02-05 17:31:57 +0530

    Create playingwithchar.c

---
+++ HackerRank/C++/Intro-To-c/playingwithchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    printf("C\n");
+     printf("Language\n");
+      printf("Welcome To C!!");
+        
+    return 0;
+}
