9a30f89bacb8efe5e4c0392e45d70c1c33c5905e
Gaurav Verma <114090891+briskgaurav@users.noreply.github.com>
2023-02-05 17:32:31 +0530

    Create sum&diff.c

---
+++ HackerRank/C++/Intro-To-c/sum&diff.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+    float x,z;
+    scanf("%d" "%d", &a, &b);
+    scanf("%f" "%f" , &x, &z);
+    int sum = a+b;
+    int diff = a-b;
+    printf("%d" "%d",&sum &diff );
+    
+    
+    return 0;
+}
