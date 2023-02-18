f5c17ca29c5a0fb18c090691713a1bd836af338d
Gaurav Verma <114090891+briskgaurav@users.noreply.github.com>
2023-02-18 14:48:21 +0530

    Create BatteryHealth.cpp

---
+++ CodeChef/BatteryHealth.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int x;
+cin>>x;
+for(int i=0;i<x;i++){
+    for(int j=0;j<x;j++){
+        if(x<=80)
+        cout<<"YES"<<endl;
+        else{
+            cout<<"NO";
+        }
+    }
+}
+	return 0;
+}
